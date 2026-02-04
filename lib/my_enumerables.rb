9163df2cb30f1cffe01ab7191fef0efee0ab0d8a
Shabir Ahmad <shabirahmadubuntu@gmail.com>
2026-02-04 10:01:15 +0000

    Complete project custom_enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,66 @@
 module Enumerable
   # Your code goes here
+  def my_select
+    return to_enum(:my_select) unless block_given?
+    new_arr = []
+    self.my_each do |elem|
+      new_arr.push elem if yield(elem)  
+    end
+    new_arr
+  end
+
+  def my_all?
+    return to_enum(:my_all?) unless block_given?
+    self.my_each do |elem|
+      return false unless yield(elem)
+    end
+    return true
+  end
+
+  def my_any?
+    return to_enum(:my_any?) unless block_given?
+    self.my_each do |elem|
+      return true if yield elem
+    end
+    return false
+  end
+
+  def my_none?
+    return to_enum(:my_none?) unless block_given?
+    self.my_each do |elem|
+      return false if yield elem
+    end
+    return true
+  end
+
+  def my_count
+    return self.length unless block_given?
+    count=0
+    self.my_each do |elem|
+      count =  count + 1 if yield elem
+    end
+    count
+  end
+
+  def my_map
+    return to_enum(:my_map)  unless block_given?
+    new_arr = []
+    self.my_each do|elem|
+     new_arr.push yield elem
+    end
+    new_arr
+  end
+
+  def my_inject(initial = :not_given)
+    return to_enum(:my_inject) unless block_given?
+     new_arr = (initial.eql? :not_given)? self : [initial] + self 
+     accumulator = new_arr[0] 
+     new_arr[1..].my_each do |elem|
+       accumulator =yield accumulator,elem
+     end
+     accumulator
+  end
+  
 end
 
 # You will first have to define my_each
@@ -8,4 +69,11 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+    self.each do|elem|
+      yield elem
+    end
+    self
+  end
 end
